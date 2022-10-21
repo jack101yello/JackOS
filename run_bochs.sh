9d5aaf2956aa5bb6dcfebd934b3b7e995ca71d13
jack101yello <jack.hudson12@yahoo.com>
2022-10-21 12:19:07 -0400

    Bootable OS

--- run_bochs.sh
+++ run_bochs.sh
@@ -1,5 +1,24 @@
 # Sets up a loopback device, runs Bochs on it, then disconnects
 
-sudo /sbin/losetup /dev/loop0 floppy.img
-sudo bochs -f bochsrc.txt
-sudo /sbin/losetup -d /dev/loop0
+LOOP=$(cat tempstorage) # Find the loop that was chosen earlier
+
+echo "Setting up loop on: $LOOP"
+sudo /sbin/losetup $LOOP floppy.img # Set up the loop
+
+# Generate bochsrc.txt
+echo "megs: 32" > bochsrc.txt
+echo "romimage: file=/usr/share/bochs/BIOS-bochs-latest, address=0xe0000" >> bochsrc.txt
+echo "vgaromimage: file=/usr/share/bochs/VGABIOS-elpin-2.40" >> bochsrc.txt
+echo "floppya: 1_44=$LOOP, status=inserted" >> bochsrc.txt
+echo "boot: a" >> bochsrc.txt
+echo "log: bochsout.txt" >> bochsrc.txt
+echo "mouse: enabled=0" >> bochsrc.txt
+echo "clock: sync=realtime" >> bochsrc.txt
+echo "cpu: ips=1000000" >> bochsrc.txt
+
+sudo bochs -f bochsrc.txt # Run bochs
+sudo /sbin/losetup -d $LOOP # End the loop
+
+# Remove temporary files
+rm tempstorage
+rm bochsrc.txt
--- update_image.sh
+++ update_image.sh
@@ -1,7 +1,12 @@
 # Puts kernel binary into the floppy image file
 
-sudo losetup /dev/loop0 floppy.img
-sudo mount /dev/loop /mnt
-sudo cp src/kernel /mnt/kernel
-sudo umount /dev/loop0
-sudo losetup -d /dev/loop0
+echo "Finding free loop"
+sudo losetup -f > tempstorage # Finds a free loop and stores it for later
+LOOP=$(cat tempstorage) # Variable to store the loop to use
+echo "Loop found: $LOOP"
+
+sudo /sbin/losetup $LOOP floppy.img
+sudo mount $LOOP /mnt2
+sudo cp -v src/kernel /mnt2/kernel
+sudo umount $LOOP
+sudo /sbin/losetup -d $LOOP
