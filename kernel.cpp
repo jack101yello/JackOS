6d926c2f5da6b4314de73463082f1f9121574ec6
jack101yello <jack.hudson12@yahoo.com>
2024-10-17 12:44:18 -0400

    Constructors

--- kernel.cpp
+++ kernel.cpp
@@ -1,12 +1,23 @@
+#include "types.h"
+
 void printf(char* str) {
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
     for(int i = 0; str[i] != '\0'; ++i) {
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
     }
 }
 
-extern "C" void kernel_main(void* multiboot_structure, unsigned int magicnumber) {
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors() {
+    for(constructor* i = &start_ctors; i != &end_ctors; i++) {
+        (*i)();
+    }
+}
+
+extern "C" void kernel_main(void* multiboot_structure, uint32_t magicnumber) {
     printf("Hello, World!");
     for(;;); // Infinite loop
 }
