9b9b9b7dec9db330ce0fa4befbd9b3418e2d233e
jack101yello <jack.hudson12@yahoo.com>
2024-10-17 16:37:08 -0400

    Implement Ports and Amend Printing

--- kernel.cpp
+++ kernel.cpp
@@ -1,11 +1,36 @@
 #include "types.h"
 #include "gdt.h"
 
-void printf(char* str) {
+void printf(const char* str) {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
+    static uint8_t x = 0, y = 0;
+
     for(int i = 0; str[i] != '\0'; ++i) {
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+        switch(str[i]) {
+            case '\n':
+                y++;
+                x = 0;
+                break;
+            default:
+                VideoMemory[80*y + x] = (VideoMemory[80*y + x] & 0xFF00) | str[i];
+                x++;
+                break;
+        }
+
+        if(x >= 80) { // Newline
+            y++;
+            x = 0;
+        }
+        if(y >= 25) { // Scrolling
+            for(y = 0; y < 25; y++) {
+                for(x = 0; x < 80; x++) {
+                    VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+                }
+            }
+            x = 0;
+            y = 0;
+        }
     }
 }
 
@@ -19,8 +44,9 @@ extern "C" void callConstructors() {
 }
 
 extern "C" void kernel_main(void* multiboot_structure, uint32_t magicnumber) {
-    printf("Hello, World!");
+    printf("Initializing JackOS Kernel\n");
 
+    printf("Setting up GDT.\n");
     GlobalDescriptorTable gdt;
 
     for(;;); // Infinite loop
